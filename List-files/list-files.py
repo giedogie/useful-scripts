986a5f2c9129b357507b8b744c5eddc491cf4737
neon <adriangryga@gmail.com>
2023-12-14 21:23:25 +0100

    List update

--- List-files/list-files.py
+++ List-files/list-files.py
@@ -25,7 +25,7 @@ def end():
     print(end_separator)
 
 # Function to list files in a directory based on different options
-def list_files_in_dir(path, sort_option='size', reverse_sort=False, date_sort_reverse=False, extension=None, date_filter=None, min_size=None, max_size=None):
+def list_files_in_dir(path, sort_option='size', reverse_sort=False, date_sort_reverse=False, extension=None, date_filter=None, phrase_filter=None):
     if not os.path.exists(path):
         print(f"Path '{path}' does not exist.")
         return
@@ -34,8 +34,7 @@ def list_files_in_dir(path, sort_option='size', reverse_sort=False, date_sort_re
         if extension is None or path.endswith(extension):
             file_size = os.path.getsize(path)
             file_size_mb = file_size / (1024 * 1024)
-            if (min_size is None or file_size_mb >= min_size) and (max_size is None or file_size_mb <= max_size):
-                print(f"File: {path}, Size: {file_size_mb:.2f} MB")
+            print(f"File: {path}, Size: {file_size_mb:.2f} MB")
 
     elif os.path.isdir(path):
         print(f"Directory: {path}")
@@ -47,14 +46,16 @@ def list_files_in_dir(path, sort_option='size', reverse_sort=False, date_sort_re
                 if extension is None or item_path.endswith(extension):
                     file_size = os.path.getsize(item_path)
                     file_size_mb = file_size / (1024 * 1024)
-                    if (min_size is None or file_size_mb >= min_size) and (max_size is None or file_size_mb <= max_size):
-                        file_created_time = os.path.getctime(item_path)
-                        file_created_time_formatted = datetime.datetime.fromtimestamp(file_created_time).strftime("%Y-%m-%d")
-                        file_list.append((item_path, file_size_mb, file_created_time_formatted))
+                    file_created_time = os.path.getctime(item_path)
+                    file_created_time_formatted = datetime.datetime.fromtimestamp(file_created_time).strftime("%Y-%m-%d")
+                    file_list.append((item_path, file_size_mb, file_created_time_formatted))
 
         if date_filter:
             start_date, end_date = date_filter
             file_list = [file_info for file_info in file_list if start_date <= file_info[2] <= end_date]
+
+        if phrase_filter:
+            file_list = [file_info for file_info in file_list if phrase_filter.lower() in os.path.basename(file_info[0]).lower()]
 
         if sort_option == 'size':
             # Sort file list by size
@@ -65,6 +66,9 @@ def list_files_in_dir(path, sort_option='size', reverse_sort=False, date_sort_re
         elif sort_option == 'extension':
             # Sort file list by extension
             file_list.sort(key=lambda x: os.path.splitext(x[0])[-1])
+        elif sort_option == 'alphabetical':
+            # Sort file list alphabetically
+            file_list.sort(key=lambda x: os.path.basename(x[0]), reverse=reverse_sort)
 
         # Print sorted list
         for file_info in file_list:
@@ -100,28 +104,17 @@ def get_specific_day():
     specific_date = input(f"Enter a specific date in format {GREEN}'YYYY-MM-DD'{RESET}: \n")
     return specific_date
 
-# Function to get user input for minimum and maximum file size
-def get_file_size_range():
-    min_size = input(f"Enter minimum file size in MB (or press {RED}Enter{RESET} to skip): ")
-    max_size = input(f"Enter maximum file size in MB (or press {RED}Enter{RESET} to skip): ")
-    try:
-        min_size = float(min_size) if min_size else None
-        max_size = float(max_size) if max_size else None
-        return min_size, max_size
-    except ValueError:
-        print(f"{RED}Invalid input. Please enter a valid number or press {RESET}{RED}Enter{RESET} to skip.")
-        return get_file_size_range()
-
-# Main loop for user interaction
 while True:
     print(f"{GREEN}This script recursively searches directories and files in defined location and prints the sorted file list.{RESET}")
     print(f"{GREEN}MENU:{RESET}")
     print(f"{GREEN}(1){RESET} List files by size")
     print(f"{GREEN}(2){RESET} List files by date")
     print(f"{GREEN}(3){RESET} List files by extension")
-    print(f"{GREEN}(4){RESET} Exit")
+    print(f"{GREEN}(4){RESET} List files by alphabetical order")
+    print(f"{GREEN}(5){RESET} List files containing phrase")
+    print(f"{GREEN}(6){RESET} Exit")
     
-    choice = input(f"Enter your choice {GREEN}(1){RESET}/{GREEN}(2){RESET}/{GREEN}(3){RESET}/{GREEN}(4){RESET}: ")
+    choice = input(f"Enter your choice {GREEN}(1){RESET}/{GREEN}(2){RESET}/{GREEN}(3){RESET}/{GREEN}(4){RESET}/{GREEN}(5){RESET}/{GREEN}(6){RESET}: ")
     
     if choice == '1':
         sort_option = 'size'
@@ -137,9 +130,8 @@ while True:
             date_filter = (specific_date, specific_date)
         else:
             date_filter = None
-        min_size, max_size = get_file_size_range()
         start()
-        list_files_in_dir(dir_path, sort_option, reverse_sort, date_sort_reverse=False, extension=extension, date_filter=date_filter, min_size=min_size, max_size=max_size)
+        list_files_in_dir(dir_path, sort_option, reverse_sort, date_sort_reverse=False, extension=extension, date_filter=date_filter)
         end()
     elif choice == '2':
         sort_option = 'date'
@@ -155,13 +147,12 @@ while True:
         else:
             date_filter = None
         extension = None  # Removed the extension prompt in this option
-        min_size, max_size = get_file_size_range()
         start()
-        list_files_in_dir(dir_path, sort_option, reverse_sort=False, date_sort_reverse=date_sort_reverse, extension=extension, date_filter=date_filter, min_size=min_size, max_size=max_size)
+        list_files_in_dir(dir_path, sort_option, reverse_sort=False, date_sort_reverse=date_sort_reverse, extension=extension, date_filter=date_filter)
         end()
     elif choice == '3':
         sort_option = 'extension'
-        dir_path = input(f"Type in the {GREEN}path{RESET} that you want to check: \n")
+        dir_path = input(f"Type in the {RED}path{RESET} that you want to check: \n")
         extension = input(f"Enter file extension {GREEN}(e.g., '.txt'){RESET} to filter by extension or leave blank to list all files: \n").lower()
         date_filter_option = get_date_filter_option()
         if date_filter_option == 'r':
@@ -172,11 +163,34 @@ while True:
             date_filter = (specific_date, specific_date)
         else:
             date_filter = None
-        min_size, max_size = get_file_size_range()
         start()
-        list_files_in_dir(dir_path, sort_option, reverse_sort=False, date_sort_reverse=False, extension=extension, date_filter=date_filter, min_size=min_size, max_size=max_size)
+        list_files_in_dir(dir_path, sort_option, reverse_sort=False, date_sort_reverse=False, extension=extension, date_filter=date_filter)
         end()
     elif choice == '4':
+        sort_option = 'alphabetical'
+        dir_path = input(f"Type in the {GREEN}path{RESET} that you want to check: \n")
+        reverse_sort = input(f"Sort order ({RED}A{RESET} for A-Z / {RED}Z{RESET} for Z-A): \n").lower() == 'z'
+        extension = input(f"Enter file extension {RED}(e.g., '.txt'){RESET} to filter by extension or leave blank to list all files: \n").lower()
+        date_filter_option = get_date_filter_option()
+        if date_filter_option == 'r':
+            start_date, end_date = get_date_range()
+            date_filter = (start_date, end_date)
+        elif date_filter_option == 'd':
+            specific_date = get_specific_day()
+            date_filter = (specific_date, specific_date)
+        else:
+            date_filter = None
+        start()
+        list_files_in_dir(dir_path, sort_option, reverse_sort, date_sort_reverse=False, extension=extension, date_filter=date_filter)
+        end()
+    elif choice == '5':
+        sort_option = 'phrase'
+        dir_path = input(f"Type in the {GREEN}path{RESET} that you want to check: \n")
+        phrase = input(f"Enter the phrase to search for in file names: \n")
+        start()
+        list_files_in_dir(dir_path, sort_option, phrase_filter=phrase)
+        end()
+    elif choice == '6':
         break
     else:
-        print(f"{RED}Invalid choice.{RESET} Please enter a valid option {GREEN}(1){RESET}/{GREEN}(2){RESET}/{GREEN}(3){RESET}/{GREEN}(4){RESET}.")
+        print(f"{RED}Invalid choice.{RESET} Please enter a valid option {GREEN}(1){RESET}/{GREEN}(2){RESET}/{GREEN}(3){RESET}/{GREEN}(4){RESET}/{GREEN}(5){RESET}/{GREEN}(6){RESET}.")
